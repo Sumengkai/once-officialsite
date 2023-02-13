48f6421f7dac55418c5f3c99ad636d32c9240e98
Sumengkai <abc880822>
2023-02-13 17:53:28 +0800

    2023/02/13 #test3

--- src/main/java/com/once/officialsite/OnceOfficialsiteApplication.java
+++ src/main/java/com/once/officialsite/OnceOfficialsiteApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnceOfficialsiteApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(OnceOfficialsiteApplication.class, args);
 	}
